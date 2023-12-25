3cec97c2ca11370324a8cd0dbec59ad56d644d14
yibxxn <yibxn23@gmail.com>
2023-12-25 16:49:39 +0800

    Added 3d model of PC

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         </div>
         <div>
         <h1 className={`${styles.heroHeadText}text-white`}>
-        Hi, I'm <span className="text-[#962BB8]">Yi Bin</span>
+        Hi, I'm <span className="text-[#915eff]">Yi Bin</span>
       </h1>
       <p className={`${styles.heroSubText} mt-2
       text-white-100`}>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           >
             <img src={logo} alt="logo" className = "w-9 h-9 object-contain" />
             <p className="text-white text-[18px] font-bold cursor-pointer flex ">YiBin 
-            &nbsp; <span className="sm:block hidden">| Software Developer</span></p>
+            &nbsp; <span className="sm:block hidden">| Developer</span></p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
