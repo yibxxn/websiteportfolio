a54d92075fd876d5b5b99fe80f8d5e93a0427fd7
yibxxn <yibxn23@gmail.com>
2024-01-02 22:05:08 +0800

    Added section wrappers and animation to sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,8 @@ import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
+import {SectionWrapper } from '../hoc'
+
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
@@ -20,7 +22,9 @@ const ServiceCard = ({ index, title, icon }) => {
             speed: 450
           }}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-        >
+        > 
+          <img src ={icon} alt={title} className="w-16 h-16 object-contain"/>
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
         </div>
       </motion.div>
     </Tilt>
@@ -56,4 +60,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
